90580877f65991821b7c07439106d30964362dce
SGreenland <78597962+SGreenland@users.noreply.github.com>
2024-07-02 17:04:20 +0100

    setinterval better?

--- src/components/Scale.vue
+++ src/components/Scale.vue
@@ -7,38 +7,24 @@
     }"
   >
     <div
-      v-if="notesAreReady"
       v-for="(note, index) in scaleToDisplay"
       :key="index"
       ref="notes"
-      @click="!audioIsPlaying && playNote(note.audioSrc, index)"
+      @click="!audioIsPlaying && playNote(index)"
       class="p-2 bg-blue-100 rounded-lg w-full h-full flex items-center justify-center cursor-pointer"
       :style="{
         gridRowStart: scaleToDisplay.length - originalOrder.indexOf(note.name),
         gridColumnStart: index + 1,
       }"
     >
-      <span
-        >{{ getNoteName(note.name)
-        }}<small>({{ originalOrder.indexOf(note.name) + 1 }})</small></span
-      >
+      <span>{{ getNoteName(note.name) }}<small>({{ originalOrder.indexOf(note.name) + 1 }})</small></span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onUpdated } from "vue";
-import type { ComputedRef } from "vue";
-import { Note } from "../types";
-import scaleManipulator from "../utils/scaleManipulator";
-
-const { reverseScale } = scaleManipulator();
-
-const notesAreReady = ref(false);
-
-onUpdated(() => {
-    notesAreReady.value = true;
-});
+import { ref, computed, onMounted, nextTick, watch } from "vue";
+import type { Note } from "../types";
 
 function getNoteName(note: string) {
   if (note.includes("sharp")) {
@@ -53,59 +39,65 @@ const props = defineProps<{
   tempo: string;
 }>();
 
-const notes = ref<any[]>();
+const notes = ref<HTMLDivElement[]>([]);
 const audioIsPlaying = ref(false);
+const audioElements = ref<HTMLAudioElement[]>([]);
 
-// const animationInterval = computed(() => 60000 / +props.tempo);
+const animationInterval = computed(() => 60000 / +props.tempo);
 
-const playNote = (audioSrc: string, index: number) => {
-  console.log(index);
-  // add animation to note
-  // notes.value?.findIndex((el, i) => {
-  //   if (i === index) {
-  //     el.style.animation = `bounce ${animationInterval.value}ms ease-out`;
-  //     setTimeout(() => {
-  //       el.style.animation = "";
-  //     }, animationInterval.value);
-  //   }
-  // });
-  const audio = new Audio(audioSrc);
+const playNote = (index: number) => {
+  // Add animation to note
+  const noteElement = notes.value[index];
+  if (noteElement) {
+    noteElement.style.animation = `bounce ${animationInterval.value}ms ease-out`;
+    setTimeout(() => {
+      noteElement.style.animation = "";
+    }, animationInterval.value);
+  }
+
+  const audio = audioElements.value[index];
+  audio.currentTime = 0; // Ensure the audio plays from the start
   audio.play();
 };
 
-let sequences: number[] = [];
-const reversedScaleMinusFirstNote: ComputedRef<Note[]> = computed(() =>
-  reverseScale(props.scaleToDisplay).slice(1)
-);
+const preloadAudio = () => {
+  audioElements.value = props.scaleToDisplay.map(note => {
+    const audio = new Audio(note.audioSrc);
+    audio.preload = "auto";
+    return audio;
+  });
+};
+
+onMounted(() => {
+  nextTick(preloadAudio);
+});
+
+watch(() => props.scaleToDisplay, preloadAudio, { deep: true });
+
+let intervalId: number | undefined;
 
 const toggleAudio = (isForwardsAndBackwards: boolean, shouldLoop: boolean) => {
   if (!audioIsPlaying.value) {
     audioIsPlaying.value = true;
-    props.scaleToDisplay.forEach((note, index) => {
-      const timeoutId = setTimeout(() => {
-        playNote(note.audioSrc, index);
-      }, index * (60000 / +props.tempo));
-      sequences.push(timeoutId);
-    });
-    if (isForwardsAndBackwards) {
-      reversedScaleMinusFirstNote.value.forEach((note, index) => {
-        const timeoutId = setTimeout(() => {
-          // get the correct note index
-          const noteIndex = props.scaleToDisplay.length - 2 - index;
-          playNote(note.audioSrc, noteIndex);
-        }, index * (60000 / +props.tempo) + props.scaleToDisplay.length * (60000 / +props.tempo));
-        sequences.push(timeoutId);
-      });
-    }
-    // need to set audio playing as false once all notes have played
-    const timeoutId = setTimeout(() => {
-      shouldLoop ? (audioIsPlaying.value = false, toggleAudio(isForwardsAndBackwards, shouldLoop)) : audioIsPlaying.value = false;
-    }, props.scaleToDisplay.length * (60000 / +props.tempo) * (isForwardsAndBackwards ? 2 : 1));
-    sequences.push(timeoutId);
-  } else { // stop audio
-    sequences.forEach((timeoutId) => {
-      clearTimeout(timeoutId);
-    });
+    let currentIndex = 0;
+
+    intervalId = setInterval(() => {
+      if (currentIndex < props.scaleToDisplay.length) {
+        playNote(currentIndex);
+        currentIndex++;
+      } else if (isForwardsAndBackwards && currentIndex < props.scaleToDisplay.length * 2 - 1) {
+        const reversedIndex = currentIndex - props.scaleToDisplay.length;
+        playNote(props.scaleToDisplay.length - 2 - reversedIndex);
+        currentIndex++;
+      } else if (shouldLoop) {
+        currentIndex = 0;
+      } else {
+        clearInterval(intervalId);
+        audioIsPlaying.value = false;
+      }
+    }, animationInterval.value);
+  } else {
+    clearInterval(intervalId);
     audioIsPlaying.value = false;
   }
 };
