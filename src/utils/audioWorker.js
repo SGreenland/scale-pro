40625fbbc026ce8a260c84396505b7ef2284784a
SGreenland <78597962+SGreenland@users.noreply.github.com>
2024-07-03 15:42:47 +0100

    ok maybe this will be the answer to my prayers

--- src/utils/audioWorker.js
+++ src/utils/audioWorker.js
@@ -1,10 +1,9 @@
-// audioWorker.js
 let currentIndex = 0;
 let isForwardsAndBackwards = false;
 let shouldLoop = false;
 let scaleLength = 0;
 let animationInterval = 1000;
-let timeoutId;
+let isRunning = false;
 
 self.onmessage = function (event) {
   const data = event.data;
@@ -15,26 +14,43 @@ self.onmessage = function (event) {
     shouldLoop = data.shouldLoop;
     scaleLength = data.scaleLength;
     animationInterval = data.animationInterval;
-    playSequence();
+    isRunning = true;
+    lastFrameTime = performance.now();
+    playSequence(lastFrameTime);
   } else if (data.type === "stop") {
-    clearTimeout(timeoutId);
+    isRunning = false;
   }
 };
 
-function playSequence() {
-  if (currentIndex < scaleLength) {
-    postMessage({ type: "playNote", index: currentIndex });
-    currentIndex++;
-  } else if (isForwardsAndBackwards && currentIndex < scaleLength * 2 - 1) {
-    const reversedIndex = currentIndex - scaleLength;
-    postMessage({ type: "playNote", index: scaleLength - 2 - reversedIndex });
-    currentIndex++;
-  } else if (shouldLoop) {
-    currentIndex = 0;
-  } else {
-    postMessage({ type: "done" });
-    return;
+function playSequence(timestamp) {
+  if (!isRunning) return;
+
+  const elapsed = timestamp - lastFrameTime;
+
+  if (elapsed >= animationInterval) {
+    if (currentIndex < scaleLength) {
+      postMessage({ type: "playNote", index: currentIndex });
+      currentIndex++;
+    } else if (isForwardsAndBackwards && currentIndex < scaleLength * 2 - 1) {
+      const reversedIndex = currentIndex - scaleLength;
+      postMessage({ type: "playNote", index: scaleLength - 2 - reversedIndex });
+      currentIndex++;
+    } else if (shouldLoop) {
+      currentIndex = 0;
+    } else {
+      postMessage({ type: "done" });
+      return;
+    }
+    lastFrameTime = timestamp;
   }
 
-  timeoutId = setTimeout(playSequence, animationInterval);
+  if (isRunning) {
+    setTimeout(() => requestAnimationFrame(playSequence), 0);
+  }
+}
+
+function requestAnimationFrame(callback) {
+  setTimeout(() => {
+    callback(performance.now());
+  }, 1000 / 60); // Aim for 60 FPS
 }
