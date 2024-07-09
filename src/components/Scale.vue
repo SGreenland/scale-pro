813df42329ef707f902743f9e3f510402cd79614
SGreenland <78597962+SGreenland@users.noreply.github.com>
2024-07-09 21:56:09 +0100

    boom

--- src/components/Scale.vue
+++ src/components/Scale.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    :class="`grid max-sm:text-xs md:gap-2 gap-1 lg:w-2/3 w-full m-auto`"
+    class="grid max-sm:text-xs md:gap-2 gap-1 py-3 lg:w-2/3 w-full m-auto drag-select-area"
     :style="{
       gridTemplateColumns: `repeat(${scaleToDisplay.length}, minmax(0, 1fr))`,
       gridTemplateRows: `repeat(${scaleToDisplay.length}, minmax(0, 1fr))`,
@@ -11,23 +11,30 @@
       :key="index"
       ref="notes"
       @click="!audioIsPlaying && playNote(index, 0)"
-      class="p-2 bg-blue-100 rounded-lg w-full h-full flex items-center justify-center cursor-pointer dark:bg-blue-800 dark:text-white"
+      class="p-2 bg-blue-100 rounded-lg w-full h-full flex items-center justify-center cursor-pointer dark:bg-blue-800 dark:text-white selectable"
       :style="{
         gridRowStart: scaleToDisplay.length - note.interval + 1,
         gridColumnStart: index + 1,
       }"
     >
-      <span
-        >{{ getNoteName(note.name)
-        }}<small>({{ note.interval }})</small></span
+      <span class="select-none"
+        >{{ getNoteName(note.name) }}<small>({{ note.interval }})</small></span
       >
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch, onMounted, onBeforeUnmount } from "vue";
+import {
+  ref,
+  computed,
+  watch,
+  onMounted,
+  onBeforeUnmount,
+  nextTick,
+} from "vue";
 import { Note } from "../types";
+import DragSelect from "dragselect";
 
 const props = defineProps<{
   scaleToDisplay: Note[];
@@ -45,6 +52,8 @@ const animationInterval = computed(() => 60000 / +props.tempo / 1000); // Conver
 const noteIndex = ref(0);
 const direction = ref(1);
 let animationFrameId: number | null = null;
+const ds = ref<DragSelect | null>(null);
+const noteSelection = ref<Note[]>([]);
 
 function getNoteName(note: string) {
   if (note.includes("sharp")) {
@@ -86,13 +95,13 @@ const playNote = (index: number, time: number) => {
 
   const noteElement = notes.value[index];
   if (noteElement) {
-      noteElement.style.animation = `bounce ${
-        animationInterval.value * 1000
-      }ms linear 1`;
-      // remove animation after it finishes
-      setTimeout(() => {
-        noteElement.style.animation = "";
-      }, animationInterval.value * 1000);
+    noteElement.style.animation = `bounce ${
+      animationInterval.value * 1000
+    }ms linear 1`;
+    // remove animation after it finishes
+    setTimeout(() => {
+      noteElement.style.animation = "";
+    }, animationInterval.value * 1000);
   }
 };
 
@@ -117,7 +126,12 @@ const scheduleNotes = (startTime: number) => {
     // reached the end of the scale
     if (
       currentDirection === 1 &&
-      currentNoteIndex === props.scaleToDisplay.length
+      (currentNoteIndex === props.scaleToDisplay.length ||
+        currentNoteIndex ===
+          props.scaleToDisplay.indexOf(
+            noteSelection.value[noteSelection.value.length - 1]
+          ) +
+            1) // reached the end of the selection
     ) {
       if (isForwardsAndBackwards.value) {
         currentDirection = -1;
@@ -125,17 +139,26 @@ const scheduleNotes = (startTime: number) => {
       } else if (shouldLoop.value) {
         // pause for two beats before looping - maybe make this a setting
         currentTime += animationInterval.value * 2;
-        currentNoteIndex = 0;
+        currentNoteIndex = noteSelection.value.length
+          ? props.scaleToDisplay.indexOf(noteSelection.value[0])
+          : 0;
       } else {
         audioIsPlaying.value = false;
         break;
       }
-    } else if (currentDirection === -1 && currentNoteIndex === -1) {
+    } else if (
+      currentDirection === -1 &&
+      (currentNoteIndex === -1 ||
+        currentNoteIndex ===
+          props.scaleToDisplay.indexOf(noteSelection.value[0]) - 1)
+    ) {
       if (isForwardsAndBackwards.value && shouldLoop.value) {
         // pause for one beat before looping
         currentTime += animationInterval.value;
         currentDirection = 1;
-        currentNoteIndex = 0; // step forward to the next note
+        currentNoteIndex = noteSelection.value.length
+          ? props.scaleToDisplay.indexOf(noteSelection.value[0])
+          : 0;
       } else if (shouldLoop.value) {
         currentNoteIndex = props.scaleToDisplay.length - 1;
       } else {
@@ -159,7 +182,9 @@ const toggleAudio = async (forwardsAndBackwards: boolean, loop: boolean) => {
   if (!audioIsPlaying.value) {
     await resumeAudioContext();
     audioIsPlaying.value = true;
-    noteIndex.value = 0;
+    noteIndex.value = noteSelection.value.length
+      ? props.scaleToDisplay.indexOf(noteSelection.value[0])
+      : 0;
     direction.value = 1;
     scheduleNotes(audioContext.currentTime);
   } else {
@@ -174,6 +199,41 @@ const toggleAudio = async (forwardsAndBackwards: boolean, loop: boolean) => {
 onMounted(() => {
   window.addEventListener("click", resumeAudioContext, { once: true });
   window.addEventListener("mousemove", resumeAudioContext, { once: true });
+
+  nextTick(() => {
+    ds.value = new DragSelect({
+      area: document.querySelector(".drag-select-area") as HTMLElement,
+      selectables: document.querySelectorAll(
+        ".selectable"
+      ) as NodeListOf<HTMLElement>,
+      draggability: false,
+      selectedClass: "bg-blue-300",
+    });
+    ds.value?.subscribe("DS:end", (e) => {
+      const selected = e.items.map((item) =>
+        item.textContent?.replace(/\(.*\)/, "").trim()
+      );
+      noteSelection.value = selected.map((note) => {
+        return props.scaleToDisplay.find(
+          (n) => getNoteName(n.name) === note
+        ) as Note;
+      });
+
+      //sort in same order as scale
+      noteSelection.value = props.scaleToDisplay.filter((note) =>
+        noteSelection.value.includes(note)
+      );
+    });
+
+    ds.value?.subscribe("DS:unselect", (e) => {
+      const selected = e.items.map((item) =>
+        item.textContent?.replace(/\(.*\)/, "").trim()
+      );
+      noteSelection.value = noteSelection.value.filter((note) => {
+        return !selected.includes(getNoteName(note.name));
+      });
+    });
+  });
 });
 
 onBeforeUnmount(() => {
