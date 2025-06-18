1dcad2dcbf7796b7e2f04d2788cd485bdb9a1dff
SGreenland <78597962+SGreenland@users.noreply.github.com>
2025-06-19 00:47:26 +0100

    Label repeat and round trip buttons

--- src/components/AudioControls.vue
+++ src/components/AudioControls.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="w-64 flex flex-col gap-4 m-auto">
-    <div class="flex w-full justify-evenly items-center m-auto">
-      <div
-        @click="mode === 'practice' ? playBackOptions.shouldLoop = !playBackOptions.shouldLoop : null"
-        role="button"
-        class="flex center-xy py-1 px-2 size-10 rounded-lg border-2 border-teal-300"
-        :class="{ 'bg-teal-200 dark:bg-teal-400': playBackOptions.shouldLoop, 'bg-gray-200 border-gray-300 opacity-60 cursor-not-allowed' : mode === 'workout' }"
-      >
-        <font-awesome-icon :icon="faRepeat" size="lg" />
+    <div class="flex w-full justify-between items-baseline m-auto">
+      <div class="flex flex-col gap-1 justify-evenly items-center">
+        <div
+          @click="mode === 'practice' ? playBackOptions.shouldLoop = !playBackOptions.shouldLoop : null"
+          role="button"
+          class="flex center-xy py-1 px-2 size-10 rounded-lg border-2 border-teal-300"
+          :class="{ 'bg-teal-200 dark:bg-teal-400': playBackOptions.shouldLoop, 'bg-gray-200 border-gray-300 opacity-60 cursor-not-allowed' : mode === 'workout' }"
+        >
+          <font-awesome-icon :icon="faRepeat" size="lg" />
+        </div>
+        <span class="w-20">Repeat</span>
       </div>
       <button
         class="disabled:opacity-50 h-10 flex items-center drop-shadow-md focus:outline-none"
@@ -17,12 +20,15 @@
       >
         <font-awesome-icon :icon="audioIsPlaying ? faStop : faPlay" />
       </button>
-      <div
-        @click="playBackOptions.isRoundTrip = !playBackOptions.isRoundTrip"
-        class="flex center-xy flex-col py-1 px-2 size-10 rounded-lg cursor-pointer border-2 border-teal-300"
-        :class="{ 'bg-teal-200 dark:bg-teal-400': playBackOptions.isRoundTrip }"
-      >
-        <font-awesome-icon :icon="faArrowsLeftRight" />
+      <div class="flex flex-col gap-1 justify-evenly items-center">
+        <div
+          @click="playBackOptions.isRoundTrip = !playBackOptions.isRoundTrip"
+          class="flex center-xy flex-col py-1 px-2 size-10 rounded-lg cursor-pointer border-2 border-teal-300"
+          :class="{ 'bg-teal-200 dark:bg-teal-400': playBackOptions.isRoundTrip }"
+        >
+          <font-awesome-icon :icon="faArrowsLeftRight" />
+        </div>
+        <span class="w-20">Round Trip</span>
       </div>
     </div>
   </div>
