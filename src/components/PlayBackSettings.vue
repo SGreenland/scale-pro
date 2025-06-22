7ee26759e29042b8ffac947c6e5236b268bf93b2
SGreenland <78597962+SGreenland@users.noreply.github.com>
2025-06-22 02:20:32 +0100

    prevent double digits in custom loop gap input

--- src/components/PlayBackSettings.vue
+++ src/components/PlayBackSettings.vue
@@ -1,21 +1,40 @@
 <template>
-    <div class="absolute right-6 w-72 bg-white dark:bg-indigo-900 shadow rounded z-50 flex flex-col gap-2 p-4">
-        <div class="flex justify-between items-center">
-          <label class="me-1" for="loop-gap">Loop Gap </label>
-          <div>
-            <select v-model="loopGap" name="" id="loop-gap" class="w-fit px-1 h-10 me-2">
-              <option v-for="option in LoopGapOptions" :key="option" :value="option">
-                {{ option }}
-              </option>
-            </select>
-            <input v-if="loopGap === 'Custom'" type="number" v-model="loopGapCustom" class="text-center border size-10 rounded-lg" min="1" max="8" />
-          </div>
-        </div>
-        <!-- <div class="flex w-full justify-between items-center">
+  <div
+    class="absolute right-6 w-72 bg-white dark:bg-indigo-900 shadow rounded z-50 flex flex-col gap-2 p-4"
+  >
+    <div class="flex justify-between items-center">
+      <label class="me-1" for="loop-gap">Loop Gap </label>
+      <div>
+        <select
+          v-model="loopGap"
+          name=""
+          id="loop-gap"
+          class="w-fit px-1 h-10 me-2"
+        >
+          <option
+            v-for="option in LoopGapOptions"
+            :key="option"
+            :value="option"
+          >
+            {{ option }}
+          </option>
+        </select>
+        <input
+          @input="($event) => { const input = $event.target as HTMLInputElement; input.value.length > 1 && (input.value = input.value[0])}"
+          v-if="loopGap === 'Custom'"
+          type="number"
+          v-model="loopGapCustom"
+          class="text-center border size-10 rounded-lg"
+          min="1"
+          max="8"
+        />
+      </div>
+    </div>
+    <!-- <div class="flex w-full justify-between items-center">
           <label for="auto-shuffle">Auto Shuffle </label>
           <toggle-switch id="auto-shuffle" v-model="autoShuffle"></toggle-switch>
         </div> -->
-      </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -23,6 +42,5 @@
 import { LoopGapOption } from "../types";
 import { loopGap, loopGapCustom } from "../GlobalState";
 
-const LoopGapOptions: LoopGapOption[] = ['Auto', 'None', 'Custom'];
-
+const LoopGapOptions: LoopGapOption[] = ["Auto", "None", "Custom"];
 </script>
