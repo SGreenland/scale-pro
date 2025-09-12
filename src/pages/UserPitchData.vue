c1fc94ef432c27a56e300bc8bf32ca3584caa5ef
SGreenland <78597962+SGreenland@users.noreply.github.com>
2025-09-12 18:28:31 +0100

    add padding and rotate y axis on pitch data chart

--- src/pages/UserPitchData.vue
+++ src/pages/UserPitchData.vue
@@ -23,11 +23,17 @@
 </template>
 
 <script setup lang="ts">
-import { AgGridVue } from "ag-grid-vue3";
-import { AgCharts } from 'ag-charts-vue3';
 import type { AgChartOptions } from "ag-charts-community";
+import { AgCharts } from "ag-charts-vue3";
+import { AgGridVue } from "ag-grid-vue3";
 import axios from "axios";
-import { computed, onBeforeMount, onBeforeUnmount, onMounted, ref, watch } from "vue";
+import {
+  computed,
+  onBeforeMount,
+  onBeforeUnmount,
+  onMounted,
+  ref
+} from "vue";
 import Checkbox from "../components/reuseable/Checkbox.vue";
 
 const shouldHideColumn = ref(false); // Set to true to hide certain columns
@@ -38,8 +44,8 @@ const allColumnsVisible = computed({
   },
 });
 const updateColumnVisibility = () => {
-    shouldHideColumn.value = window.innerWidth < 640;
-  };
+  shouldHideColumn.value = window.innerWidth < 640;
+};
 
 onMounted(() => {
   // hide certain columns on small screens
@@ -70,7 +76,13 @@ const colDefs = ref([
     type: "date",
     flex: 1,
   },
-  { headerName: "Scale", field: "scale", sortable: true, filter: true, flex: 1 },
+  {
+    headerName: "Scale",
+    field: "scale",
+    sortable: true,
+    filter: true,
+    flex: 1,
+  },
   {
     headerName: "In Tune",
     field: "percentageInTune",
@@ -99,59 +111,68 @@ const colDefs = ref([
 ]);
 
 const averageScorePerScale = computed(() => {
-    const scaleMap: Record<string, { total: number; count: number }> = {};
-    rowData.value.forEach((entry: any) => {
-        if (!scaleMap[entry.scale]) {
-            scaleMap[entry.scale] = { total: 0, count: 0 };
-        }
-        scaleMap[entry.scale].total += entry.percentageInTune;
-        scaleMap[entry.scale].count += 1;
-    });
-    return Object.entries(scaleMap).map(([scale, data]) => ({
-        Scale: scale,
-        'In tune %': data.total / data.count,
-    }))
+  const scaleMap: Record<string, { total: number; count: number }> = {};
+  rowData.value.forEach((entry: any) => {
+    if (!scaleMap[entry.scale]) {
+      scaleMap[entry.scale] = { total: 0, count: 0 };
+    }
+    scaleMap[entry.scale].total += entry.percentageInTune;
+    scaleMap[entry.scale].count += 1;
   });
+  return Object.entries(scaleMap).map(([scale, data]) => ({
+    Scale: scale,
+    "In tune %": data.total / data.count,
+  }));
+});
 
-
- const options = computed<AgChartOptions>(() => ({
-            // Data: Data to be displayed in the chart
-            data: averageScorePerScale.value,
-            title: {
-                text: 'Average In Tune Percentage by Scale',
-                fontSize: 18,
-                fontFamily: 'Arial, sans-serif',
-            },
-            // Series: Defines which chart type and data to use
-            series: [{ type: 'bar', direction: 'horizontal', xKey: 'Scale', yKey: 'In tune %', fill: '#4F46E5', cornerRadius: 10 }],
-            axes: [
-                {
-                    type: 'category',
-                    position: 'left',
-                    paddingOuter: 0.7,
-                    label: {
-                        rotation: 0,
-                        fontSize: 14,
-                        fontFamily: 'Arial, sans-serif',
-                    },
-                },
-                {
-                    type: 'number',
-                    position: 'bottom',
-                    label: {
-                        fontSize: 14,
-                        fontFamily: 'Arial, sans-serif',
-                        formatter: (params) => `${Math.round(params.value)}%`,
-                    },
-                    min: 0,
-                    max: 100,
-                    tickInterval: 10,
-                },
-            ],
-
- }));
-
-
+const options = computed<AgChartOptions>(() => ({
+  // Data: Data to be displayed in the chart
+  data: averageScorePerScale.value,
+  title: {
+    text: "Average In Tune Percentage by Scale",
+    fontSize: 18,
+    fontFamily: "Arial, sans-serif",
+  },
+  // Series: Defines which chart type and data to use
+  series: [
+    {
+      type: "bar",
+      direction: "horizontal",
+      xKey: "Scale",
+      yKey: "In tune %",
+      fill: "#4F46E5",
+      cornerRadius: 10,
+    },
+  ],
+  padding: {
+    right: 30,
+    left: 10,
+  },
+  axes: [
+    {
+      type: "category",
+      position: "left",
+      paddingOuter: 0.7,
+      label: {
+        rotation: -45,
+        fontSize: 14,
+        fontFamily: "Arial, sans-serif",
+      },
+    },
+    {
+      type: "number",
+      position: "bottom",
+      label: {
+        fontSize: 14,
+        fontFamily: "Arial, sans-serif",
+        formatter: (params) => `${Math.round(params.value)}%`,
+      },
+      min: 0,
+      max: 100,
+      tickInterval: 10,
+    },
+  ],
+}));
 
 onBeforeMount(() => {
   axios
@@ -168,7 +189,7 @@ onBeforeMount(() => {
 
 <style>
 span.ag-paging-page-size {
-    display: none !important;
+  display: none !important;
 }
 .chart-wrapper {
   border-radius: 8px;
