b4d44f1f8af5c0506f2771dad796456a9b1d534c
SGreenland <78597962+SGreenland@users.noreply.github.com>
2025-09-26 17:31:46 +0100

    change dark mode note color

--- src/components/Scale.vue
+++ src/components/Scale.vue
@@ -50,7 +50,7 @@
       ></div>
       <div
         v-if="ghostNote"
-        class="fixed pointer-events-none z-50 rounded-2xl px-3 py-2 bg-sky-200 border border-indigo-500 shadow-lg text-black dark:bg-cyan-700 dark:text-white opacity-80"
+        class="fixed pointer-events-none z-50 rounded-2xl px-3 py-2 bg-sky-200 border border-indigo-500 shadow-lg text-black dark:bg-fuchsia-800 dark:text-white opacity-80"
         :style="{
           top: ghostNote.y + 'px',
           left: ghostNote.x + 'px',
@@ -74,7 +74,7 @@
         @touchend="handleDragOrTouchEnd"
         @click="playNote(index, 0)"
         :id="index.toString()"
-        class="bg-sky-200 border border-indigo-500 shadow rounded-2xl size-full flex items-center justify-center cursor-pointer dark:bg-cyan-700 dark:text-white"
+        class="bg-sky-200 border border-indigo-500 shadow rounded-2xl size-full flex items-center justify-center cursor-pointer dark:bg-fuchsia-800 dark:text-white"
         :class="{
           'relative bottom-4 w-1/2 h-5/6 m-auto z-50':
             currentSettings.gridType === 'Guitar tab',
